9bfce8273d5b2d8c727cd3c8c2e9c4738ae1502f
TianaY320 <63749917+TianaY320@users.noreply.github.com>
2020-09-29 21:28:49 -0400

    Slight visual adjustments

--- Assets/Script/Bow.cs
+++ Assets/Script/Bow.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class Bow : MonoBehaviour
 {
+
+    [Header("Set in Inspector")]
+    public GameObject prefabArrow;
+
+    [Header("Set Dynamically")]
     public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject arrow;
+    public bool aimingMode;
 
     void Awake()
     {
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
     }
 
     void OnMouseEnter()
@@ -23,5 +32,13 @@ public class Bow : MonoBehaviour
     {
         //print("Bow:OnMouseExit()");
         launchPoint.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        aimingMode = true;
+        arrow = Instantiate(prefabArrow) as GameObject;
+        arrow.transform.position = launchPos;
+        arrow.GetComponent<Rigidbody>().isKinematic = true;
     }
 }
