48107af5b7df7d67f1fc563daea21fa0d8f94171
TianaY320 <63749917+TianaY320@users.noreply.github.com>
2020-09-30 00:09:21 -0400

    Shoot arrow

--- Assets/Script/Bow.cs
+++ Assets/Script/Bow.cs
@@ -7,12 +7,14 @@ public class Bow : MonoBehaviour
 
     [Header("Set in Inspector")]
     public GameObject prefabArrow;
+    public float velocityMult = 8f;
 
     [Header("Set Dynamically")]
     public GameObject launchPoint;
     public Vector3 launchPos;
     public GameObject arrow;
     public bool aimingMode;
+    private Rigidbody arrowRigidbody;
 
     void Awake()
     {
@@ -40,5 +42,36 @@ public class Bow : MonoBehaviour
         arrow = Instantiate(prefabArrow) as GameObject;
         arrow.transform.position = launchPos;
         arrow.GetComponent<Rigidbody>().isKinematic = true;
+        arrowRigidbody = arrow.GetComponent<Rigidbody>();
+        arrowRigidbody.isKinematic = true;
+    }
+
+    void Update()
+    {
+        if (!aimingMode) return;
+
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        Vector3 mouseDelta = mousePos3D - launchPos;
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+
+        if (mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+
+        Vector3 arrPos = launchPos + mouseDelta;
+        arrow.transform.position = arrPos;
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            aimingMode = false;
+            arrowRigidbody.isKinematic = false;
+            arrowRigidbody.velocity = -mouseDelta * velocityMult;
+            FollowCam.POI = arrow;
+            arrow = null;
+        }
     }
 }
