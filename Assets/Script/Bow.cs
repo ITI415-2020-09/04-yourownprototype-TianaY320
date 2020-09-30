fd907ee15459518d9663693128f103c23360a941
TianaY320 <63749917+TianaY320@users.noreply.github.com>
2020-09-29 21:04:28 -0400

    Adjusting Halo

--- Assets/Script/Bow.cs
+++ Assets/Script/Bow.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class Bow : MonoBehaviour
 {
+    public GameObject launchPoint;
+
+    void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+    }
+
     void OnMouseEnter()
     {
-        print("Bow:OnMouseEnter()");
+        //print("Bow:OnMouseEnter()");
+        launchPoint.SetActive(true);
     }
 
     void OnMouseExit()
     {
-        print("Bow:OnMouseExit()");
+        //print("Bow:OnMouseExit()");
+        launchPoint.SetActive(false);
     }
 }
