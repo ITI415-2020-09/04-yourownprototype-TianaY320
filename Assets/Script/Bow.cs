5f8047669f21a55cf82f5883c5f5906d955ed070
TianaY320 <63749917+TianaY320@users.noreply.github.com>
2020-09-29 20:52:19 -0400

    Make Halo

---
+++ Assets/Script/Bow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bow : MonoBehaviour
+{
+    void OnMouseEnter()
+    {
+        print("Bow:OnMouseEnter()");
+    }
+
+    void OnMouseExit()
+    {
+        print("Bow:OnMouseExit()");
+    }
+}
